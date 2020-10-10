fa0383c0cdbd7c6181443699070da3d83dc10b4a
Grace Desi Geoloni <64132142+graaceo@users.noreply.github.com>
2020-10-10 21:19:01 +0700

    Create konversidetik.php
    cara mengkonversikan ke detik

---
+++ konversidetik.php
@@ -0,0 +1,15 @@
+<?php
+$nilai_jam = 1;
+$menit = $nilai_jam*60;
+$detik = $nilai_jam*3600;
+$jam = $nilai_jam*1;
+echo "Jumlah menit : ".$menit."<br/>"; echo "Jumlah detik : ".$detik."<br/>"; echo "Jumlah jam : ".$jam."<br/>";
+?>
+
+<?php
+$nilai_detik = 3600;
+$jam = $nilai_detik/3600;
+$menit = $nilai_detik/60;
+$detik = $nilai_detik/1;
+echo "Jumlah jam : ".$jam."<br/>"; echo "Jumlah jmenit : ".$menit."<br/>"; echo "Jumlah detik : ".$detik."<br/>";
+?>
